e71440b7577763bbab6e3fd8a4d339634ba70f4b
wagnergabe <102180936+wagnergabe@users.noreply.github.com>
2023-05-05 19:39:36 -0500

    added window.innerHeight setup

---
+++ script.js
@@ -0,0 +1,8 @@
+const boxes = document.querySelectorAll(".box");
+
+window.addEventListener('scroll', checkBoxes);
+
+function checkBoxes () {
+ const triggerBottom = (window.innerHeight / 5 * 4) 
+}
+
