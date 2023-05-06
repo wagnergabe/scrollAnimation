08f6399e2db572c05d4ccbf4bb3ae7174cedf14a
wagnergabe <102180936+wagnergabe@users.noreply.github.com>
2023-05-05 19:42:07 -0500

    getBoundingClientRect function added

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 const boxes = document.querySelectorAll(".box");
 
-window.addEventListener('scroll', checkBoxes);
+window.addEventListener("scroll", checkBoxes);
 
-function checkBoxes () {
- const triggerBottom = (window.innerHeight / 5 * 4) 
+function checkBoxes() {
+  const triggerBottom = (window.innerHeight / 5) * 4;
+
+  boxes.forEach((box) => {
+    const boxTop = box.getBoundingClientRect().top;
+
+    if (boxTop < triggerBottom) {
+      box.classList.add("show");
+    } else {
+      box.classList.remove("show");
+    }
+  });
 }
-
